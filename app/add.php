8c86e6c88bfb01fb279ea6bce338e0732df6f5ad
cyro14 <cyro.14@hotmail.com>
2022-11-09 21:27:31 -0400

    Adição de data e hora nas tarefas e implementação da inserção de tarefas
    Criação do campo "date_time" no banco de dados e adicionado ao index;
    Implementação da inserção de tarefas do front ao sql.

---
+++ app/add.php
@@ -0,0 +1,25 @@
+<?php 
+
+if(isset($_POST['descricao'])){
+    require '../db_conn.php';
+
+    $descricao = $_POST['descricao'];
+
+    if(empty($descricao)){
+        header("Location: ../index.php?mess=error");    
+    }else{
+        $stmt = $conn->prepare("INSERT INTO tarefas(descricao) VALUE(?)");
+        $stmt->execute([$descricao]);
+
+        if($res){
+            header("Location: ../index.php?mess=sucess");    
+        }else{
+            header("Location: ../index.php");    
+        }
+        $conn = null;
+        exit();
+    }
+}else{
+    header("Location: ../index.php?mess=error");
+}
+ 
